206734a7114effffcdbab3068f65b2323e56217b
sobharrison <130856961+sobharrison@users.noreply.github.com>
2024-10-26 16:03:13 -0500

    prey movement and sensing of beluga

--- Scripts/prey.gd
+++ Scripts/prey.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 5.0
+const VELOCITY_MAX = 20
 
 var timer: int = 0
+var rng
+var beluga
+var sprite
 
 func _ready():
 	self.visible = false
+	rng = RandomNumberGenerator.new()
+	beluga = self.get_parent().get_parent().find_child("Beluga2D")
+	print(beluga)
+	sprite = self.find_child("Sprite2D")
 
 func _physics_process(delta: float) -> void:
 	hide_me()
@@ -29,8 +36,33 @@ func _physics_process(delta: float) -> void:
 
 	#move_and_slide()
 	
-	var collision_info = move_and_collide(velocity * delta)
+	if self.visible:
+		# run from player
+		var player_angle = self.position.angle_to_point(beluga.position)
+		var run = Vector2.from_angle(player_angle) * SPEED
+		var temprun = run.x
+		run.x = run.y
+		run.y = temprun
+		velocity += run
+	else:
+		# safe, wander
+		velocity.x += rng.randi_range(-SPEED, SPEED)
+		velocity.y += rng.randi_range(-SPEED, SPEED)
+		# Max Velocity
+		velocity = velocity.limit_length(VELOCITY_MAX)
 	
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+		
+		# eaten by predator
+		if collision_info.get_collider().get_collision_layer() == 8:
+			eat_me()
+	
+	if velocity.x > 0:
+		sprite.flip_h = false
+	else:
+		sprite.flip_h = true
 
 func hide_me() -> void:
 	var current = Time.get_ticks_msec()
